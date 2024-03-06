004167291c2218de1a69ab27bcd7413e3aa90a41
Robin <robin.sliet@gmail.com>
2024-03-06 12:05:25 +0530

    Counter App v2

--- 03counterApp/src/App.jsx
+++ 03counterApp/src/App.jsx
@@ -1,14 +1,25 @@
 import "./App.css";
+import { useState } from "react";
 function App() {
+  let [counter, setCounter] = useState(0);
+
+  const incrementValue = () => setCounter(++counter);
+  const decrementValue = () =>
+    counter - 1 >= 0 ? setCounter(--counter) : setCounter(counter);
   return (
     <>
       <h1>Counter App</h1>
       <br />
-      <h2>Current Count : </h2>
+      <h2>Current Count : {counter}</h2>
       <br />
-      <div> <button>Add by 1</button>  <button>Reduce by 1</button></div>
+      <div>
+        {" "}
+        <button onClick={incrementValue}>Add by 1</button>{" "}
+        <button onClick={decrementValue}>Reduce by 1</button>
+        <h5>Value cant decrement below 0</h5>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
