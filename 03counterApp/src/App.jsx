2be5784c14918c92bd5819a8c9a4dd1b6fdabfc0
Robin <robin.sliet@gmail.com>
2024-03-06 11:57:56 +0530

    Lecture 3 Counter App

---
+++ 03counterApp/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+function App() {
+  return (
+    <>
+      <h1>Counter App</h1>
+      <br />
+      <h2>Current Count : </h2>
+      <br />
+      <div> <button>Add by 1</button>  <button>Reduce by 1</button></div>
+    </>
+  )
+}
+
+export default App
